4a00f03d3d2ac5fab35f2cfc172355efa39bde47
Sahil Batra <shlbatra123@gmail.com>
2026-06-23 06:59:16 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Given an integer array nums, return an array output 
+        # where output[i] is the product of all the elements of nums except nums[i]
+        # nums = [1,2,4,6]
+        # [48,24,12,8]
+        # Prefix -> [1, 2, 8, 48]
+        # Postfix -> [48, 48, 24 ,6]
+        # Approach 1 - product all and then divide by number in index - not allowed here
+        # Take into acct 0s, if >1 0 then res all 0s, if only 1 zero then that position prod and rest 0
+        # If 
+        # Approach 2 - Use Prefix * Postfix - both computed in O(n) and then iterate O(n) - Overall O(n)
+        # Space - O(n) - memory - compute prefix and postfix store directly in output array
+        # Prefix -> [1, 2, 8, 48]
+        # Postfix -> [48, 48, 24 ,6]
+        # [1*48, 1*24, 2*6, 8*1]
+        # O(n) and O(1)
+        output = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)-1):
+            prefix = prefix * nums[i]
+            output[i+1] = prefix
+        print(output)
+        postfix = 1
+        for j in range(len(nums)-1, 0, -1):
+            postfix = postfix * nums[j]
+            output[j-1] = output[j-1] * postfix
+        print(output)
+        return output
