cb1b4e8b1a18ed74c3b4b584e701df424eb1434f
Sahil Batra <shlbatra123@gmail.com>
2026-06-24 05:41:59 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # nums = [-1,0,1,2,-1,-4]
+        # [[-1,-1,2],[-1,0,1]]
+        # Approach - Sort first. 
+        # -4, -1, -1, 0, 1, 2 (skip duplicates when i move across sorted array) and also l & r window
+        # then reduces to 2 sum using hash map or hash set
+        # Or alternate left and right ptr sum > 0 then r shift left, sum < 0 then l shift right
+        # soln O(nlogn) + O(n^2)
+        nums = sorted(nums)
+        res = []
+        for i in range(len(nums)):
+
+            if nums[i] > 0: # array already sorted so if minimum is > 0 then not possible to sum to 0
+                break
+
+            if i > 0 and nums[i-1] == nums[i]: # duplicate entries
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                threesum = nums[i] + nums[l] + nums[r]
+
+                if threesum > 0:
+                    r = r - 1
+                elif threesum < 0:
+                    l = l + 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l = l + 1
+                    r = r - 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l = l + 1
+
+        return res
+
+
+
+            
+
